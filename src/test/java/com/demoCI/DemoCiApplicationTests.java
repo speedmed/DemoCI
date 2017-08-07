c38365d0140b1e44d4925a4f43a3af81a4cd99ae
Speedz Makk <speedmed@gmail.com>
2017-08-07 17:41:09 +0000

    add an experimental test

---
+++ src/test/java/com/demoCI/DemoCiApplicationTests.java
@@ -0,0 +1,42 @@
+package com.demoCI;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoCiApplicationTests {
+
+	public DemoCiApplicationTests() {
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+	}
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void contextLoads() {
+		assertEquals(1, 1);
+	}
+
+}
